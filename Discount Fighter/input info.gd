16a456d595d4ba57d64bcd1fb10432651718d7b1
Dethchum <dethcumsucks@gmail.com>
2025-06-01 02:11:25 -0500

    test

--- Discount Fighter/input info.gd
+++ Discount Fighter/input info.gd
@@ -3,7 +3,6 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_text("...")
-	if Input.is_action_pressed("left"):
-		
+	if Input.is_anything_pressed():
+		set_text("input")
 	
